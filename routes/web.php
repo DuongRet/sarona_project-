7ce6f27fba93306f6b3408446cace1b5d59aeabf
heychhorn <86866142+heychhorn@users.noreply.github.com>
2021-11-28 21:50:01 +0700

    test

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/admin/chhornhey/admin', function () {
+    return view('admin.blade.php');
+});
